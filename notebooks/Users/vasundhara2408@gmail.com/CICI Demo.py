b4a3a2e3b7b9b6b850b17c5caaa7ec5e46ac750e
GitVasu24 <103126981+GitVasu24@users.noreply.github.com>
2022-04-08 16:58:31 +0530

    New notebook

---
+++ notebooks/Users/vasundhara2408@gmail.com/CICI Demo.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %md #CICD Implementation
+
+# COMMAND ----------
+
+# MAGIC %md Step 1
+
+# COMMAND ----------
+
+a=106
+b=200
+c=a+b
+print(c)
+
+# COMMAND ----------
+
