92aec10fb74758b8eeed1aa3af73a9ea698fb0cc
GitVasu24 <103126981+GitVasu24@users.noreply.github.com>
2022-04-11 22:56:12 +0530

    0.21

---
+++ notebooks/Users/vasundhara2408@gmail.com/QA_demo.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+# MAGIC %md #my first code in QA
+
+# COMMAND ----------
+
+print("hello")
+
+# COMMAND ----------
+
+# MAGIC %sql
+
+# COMMAND ----------
+
+# MAGIC %md #new line added
+
+# COMMAND ----------
+
+select * from test
